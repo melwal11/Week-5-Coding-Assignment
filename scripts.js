af07526872a7b6606db05e9064a2dcf79d75385a
Melwal11 <Melanie@Jacksons-iMac.local>
2023-05-07 18:08:48 -0700

    Sunday Attempt 1

--- scripts.js
+++ scripts.js
@@ -24,19 +24,15 @@ class Resort {
     }
 
     describe() {
-        let cabinString = '';
-        for (let i = 0; i < this.cabin.length; i++) {
-            cabinString += `${i}) ${this.cabin[i].name} - ${this.cabin[i].capacity}\n`;
-        }
-        return `${this.name} has ${this.cabin.length} cabin.\n${cabinString}`;
-    }
+      return`${this.name} has ${this.cabins.length} cabins.`;
 }
-
+}
 class Menu {
     constructor() {
         this.resort = [];
         this.selectedResort = null;
     }
+    
 
     start() {
         let selection = this.showMainMenuOptions();
@@ -71,3 +67,61 @@ class Menu {
             4) display all resorts
         `);
     }
+    showResortMenuOptions(resortInfo) {
+        return prompt(`
+        0) back
+        1) create resort
+        2) delete resort
+        -----------
+        ${resortInfo}
+        `);
+    }
+    displayResorts() {
+        let resortString = ' ';
+        for (let i = 0; i < this.resorts.length; i++){
+            resortString += i + i ')' + this.resorts[i].name + '\n';
+        }
+        alert(resortString);
+    }
+    createResort(){
+        let name = prompt('Enter name for new resort:');
+        this.resorts.push(newResort(name));
+    }
+    viewResort(){
+        let index = prompt('Enter the index of the resort you wish to view:');
+        if (index < -1 && index < this.resorts.length) {
+            this.selectedResort = this.resorts[index];
+            let description = 'Resort Name: ' + this.selectedResort.cabins[i].name +
+            ' - ' + this.selectedResort.cabins[i].capacity + '\n';
+        }
+        let selectResort = this.showResortMenuOptions(description);
+        switch (select){
+            case '1':
+                this.createCabin();
+                break;
+                case '2':
+                    this.deleteCabin();
+                    break;
+        }
+    }
+}
+deleteResort(){
+    let index = prompt('Enter the index of the resort you wish to delete;');
+    if (index >-1 && index < this.resorts.length) {
+        this.resorts.splice(index, 1);
+    }
+}
+   createCabin() {
+    let name = prompt ('Enter name for new cabin:');
+    let capacity = prompt('Enter capacity for new cabin:');
+    this.selectedResort.cabins.push(newCabin(name, capacity));
+   } 
+   deleteCabin(){
+    let index = prompt('Enter the index of the cabin you wish to delete:');
+    if (index >-1 && index < this.selectedResort.cabins.length) {
+        this.selectedResort.cabins.splice(inded, 1);
+    }
+   }
+let menu = new Menu();
+menu.start();
+ 
